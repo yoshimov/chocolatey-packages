db81803819b98c52c6d9ce02285d1ac1e477dd5f
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-20 19:55:29 +0900

    changed groovy and scala into binroot.

--- groovy/tools/chocolateyInstall.ps1
+++ groovy/tools/chocolateyInstall.ps1
@@ -1,38 +1,23 @@
-﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
-  # installer, will assert administrative rights
-  #Install-ChocolateyPackage 'groovy' 'EXE_OR_MSI' 'SILENT_ARGS' 'URL' '64BIT_URL_DELETE_IF_NO_64BIT'  -validExitCodes @(0)
-  # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-  #Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-  # download and unpack a zip file
-  $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
-  Install-ChocolateyZipPackage 'groovy' 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.2.zip' "$parentpath"
-  
-  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
-  # downloader that the main helpers use to download items
-  #Get-ChocolateyWebFile 'groovy' 'DOWNLOAD_TO_FILE_FULL_PATH' 'URL' '64BIT_URL_DELETE_IF_NO_64BIT'
-  # installer, will assert administrative rights - used by Install-ChocolateyPackage
-  #Install-ChocolateyInstallPackage 'groovy' 'EXE_OR_MSI' 'SILENT_ARGS' '_FULLFILEPATH_' -validExitCodes @(0)
-  # unzips a file to the specified location - auto overwrites existing content
-  #Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-  #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes @(0)
-  # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
-  Install-ChocolateyPath "$parentpath\groovy-2.0.2\bin" 'User' # Machine will assert administrative rights
-  # add specific files as shortcuts to the desktop
-  #$target = Join-Path $MyInvocation.MyCommand.Definition 'groovy.exe'
-  #Install-ChocolateyDesktopLink $target
-  
-  #------- ADDITIONAL SETUP -------#
-  # make sure to uncomment the error handling if you have additional setup to do
+﻿try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
+  $package = 'groovy-2.0.2'
 
-  #$processor = Get-WmiObject Win32_Processor
-  #$is64bit = $processor.AddressWidth -eq 64
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  $zipUrl = 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.2.zip'
 
+  Install-ChocolateyZipPackage 'groovy' "$zipUrl" "$binRoot"
   
-  # the following is all part of error handling
-  #Write-ChocolateySuccess 'groovy'
-#} catch {
-  #Write-ChocolateyFailure 'groovy' "$($_.Exception.Message)"
-  #throw 
-#}
+#  Move-Item "$($installDir)\groovy-2.0.2\*" "$installDir" -Force -Recurse
+
+  Install-ChocolateyPath "$installDir\bin" 'User' # Machine will assert administrative rights
+
+  Write-ChocolateySuccess 'groovy'
+} catch {
+  Write-ChocolateyFailure 'groovy' "$($_.Exception.Message)"
+  throw 
+}
