faed7eea13bbfeab3051ab4fd02358b58513cb64
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-20 19:12:17 +0900

    changed xyzzy to use 7zip instead of lha32.

--- xyzzy/tools/chocolateyInstall.ps1
+++ xyzzy/tools/chocolateyInstall.ps1
@@ -1,36 +1,13 @@
 ﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
-  # installer, will assert administrative rights
-#  Install-ChocolateyPackage 'xyzzy' 'EXE_OR_MSI' 'SILENT_ARGS' 'URL' '64BIT_URL_DELETE_IF_NO_64BIT'  -validExitCodes @(0)
-  # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-  #Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-  # download and unpack a zip file
-#  Install-ChocolateyZipPackage 'xyzzy' 'http://www.jsdlab.co.jp/~kamei/cgi-bin/download.cgi' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  
-  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
-  # downloader that the main helpers use to download items
   $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
   Get-ChocolateyWebFile 'xyzzy' "$parentpath\xyzzy.lzh" 'http://www.jsdlab.co.jp/~kamei/cgi-bin/download.cgi'
-  # installer, will assert administrative rights - used by Install-ChocolateyPackage
-  #Install-ChocolateyInstallPackage 'xyzzy' 'EXE_OR_MSI' 'SILENT_ARGS' '_FULLFILEPATH_' -validExitCodes @(0)
-  # unzips a file to the specified location - auto overwrites existing content
-  #Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-  Start-ChocolateyProcessAsAdmin "cd $parentpath; tools\lha32.exe x xyzzy.lzh" -validExitCodes @(0)
-  # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
+  cmd /c "$parentpath\tools\extractlzh.bat"
+#  Start-ChocolateyProcessAsAdmin "cd $parentpath; tools\lha32.exe x xyzzy.lzh" -validExitCodes @(0)
   Install-ChocolateyPath "$parentpath\xyzzy" 'User' # Machine will assert administrative rights
-  # add specific files as shortcuts to the desktop
+
   $target = Join-Path $parentpath 'xyzzy\xyzzy.exe'
   Install-ChocolateyDesktopLink $target
   
-  #------- ADDITIONAL SETUP -------#
-  # make sure to uncomment the error handling if you have additional setup to do
-
-  #$processor = Get-WmiObject Win32_Processor
-  #$is64bit = $processor.AddressWidth -eq 64
-
-  
-  # the following is all part of error handling
   #Write-ChocolateySuccess 'xyzzy'
 #} catch {
   #Write-ChocolateyFailure 'xyzzy' "$($_.Exception.Message)"
---
+++ xyzzy/tools/extractlzh.bat
@@ -0,0 +1,2 @@
+cd /d "%~dp0"\..
+"%ProgramFiles%\7-zip\7z.exe" x xyzzy.lzh
