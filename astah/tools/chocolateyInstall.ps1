f5ab3baa624b49c7123e536530cc2cdb7214e68a
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-20 16:36:50 +0900

    added astah uml tool.

---
+++ astah/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
+  $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
+  Install-ChocolateyZipPackage 'astah' 'http://members.change-vision.com/files/_VIQk3oi8TeFfKD5xBG5FutLTSitwWAsM/astah_community/6_6_3/astah-community-6_6_3.zip' "$parentpath" 
+  
+  $target = Join-Path $parentpath 'astah_community\astah-com.exe'
+  Install-ChocolateyDesktopLink $target
+  
+  #Write-ChocolateySuccess 'astah'
+#} catch {
+  #Write-ChocolateyFailure 'astah' "$($_.Exception.Message)"
+  #throw 
+#}
