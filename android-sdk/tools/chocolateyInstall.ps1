e156bcc045cc858af9b3554cec7ab1898869345c
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-20 16:00:28 +0900

    added android sdk package.

---
+++ android-sdk/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
+  Install-ChocolateyPackage 'android-sdk' 'EXE' '/S' 'http://dl.google.com/android/installer_r20.0.3-windows.exe' -validExitCodes @(0)
+
+  #Write-ChocolateySuccess 'android-sdk'
+#} catch {
+  #Write-ChocolateyFailure 'android-sdk' "$($_.Exception.Message)"
+  #throw 
+#}
