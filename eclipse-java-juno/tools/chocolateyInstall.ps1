5b9aef0bcbcfcd53977ae3606f8f0e6ab629ab01
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-20 20:23:56 +0900

    changed eclipse to install into binroot. added dependencies to binroot package.

--- eclipse-java-juno/tools/chocolateyInstall.ps1
+++ eclipse-java-juno/tools/chocolateyInstall.ps1
@@ -1,36 +1,23 @@
 ﻿try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
-  # installer, will assert administrative rights
-#  Install-ChocolateyPackage 'eclipse-java-juno' 'EXE_OR_MSI' 'SILENT_ARGS' 'URL' '64BIT_URL_DELETE_IF_NO_64BIT'  -validExitCodes @(0)
-  # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-  #Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-  # download and unpack a zip file
-  $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
-  Install-ChocolateyZipPackage 'eclipse-java-juno' 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/R/eclipse-java-juno-win32.zip&r=1' "$parentpath" 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/R/eclipse-java-juno-win32-x86_64.zip&r=1' 
+  $package = 'eclipse-juno'
+
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  $zipUrl = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/R/eclipse-java-juno-win32.zip&r=1'
+  $zip64Url = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/R/eclipse-java-juno-win32-x86_64.zip&r=1'
+
+  Install-ChocolateyZipPackage 'eclipse-java-juno' "$zipUrl" "$binRoot" "$zip64Url"
   
-  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
-  # downloader that the main helpers use to download items
-  #Get-ChocolateyWebFile 'eclipse-java-juno' 'DOWNLOAD_TO_FILE_FULL_PATH' 'URL' '64BIT_URL_DELETE_IF_NO_64BIT'
-  # installer, will assert administrative rights - used by Install-ChocolateyPackage
-  #Install-ChocolateyInstallPackage 'eclipse-java-juno' 'EXE_OR_MSI' 'SILENT_ARGS' '_FULLFILEPATH_' -validExitCodes @(0)
-  # unzips a file to the specified location - auto overwrites existing content
-  #Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-  #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes @(0)
-  # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
-  #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
-  # add specific files as shortcuts to the desktop
-  $target = Join-Path $parentpath 'eclipse\eclipse.exe'
+  Rename-Item "$binRoot\eclipse" "$package" -Force
+
+  $target = Join-Path $installDir 'eclipse.exe'
   Install-ChocolateyDesktopLink $target
-  
-  #------- ADDITIONAL SETUP -------#
-  # make sure to uncomment the error handling if you have additional setup to do
 
-  #$processor = Get-WmiObject Win32_Processor
-  #$is64bit = $processor.AddressWidth -eq 64
-
-  
-  # the following is all part of error handling
   Write-ChocolateySuccess 'eclipse-java-juno'
 } catch {
   Write-ChocolateyFailure 'eclipse-java-juno' "$($_.Exception.Message)"
