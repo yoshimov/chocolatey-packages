4a37b170eba6709838c0db41452a9292e99370b6
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-21 13:06:16 +0900

    added ubuntu wubi.

---
+++ wubi/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿#try {
+  Install-ChocolateyPackage 'wubi' 'EXE' '' 'http://www.ubuntu.com/start-download?distro=wubi&bits=32&release=desktop' -validExitCodes @(0)
+
+  #Write-ChocolateySuccess 'wubi'
+#} catch {
+  #Write-ChocolateyFailure 'wubi' "$($_.Exception.Message)"
+  #throw 
+#}
