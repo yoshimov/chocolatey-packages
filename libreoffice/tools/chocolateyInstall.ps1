fdb1a3c4abde8370004dbab75cad632adf87004c
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-21 15:20:09 +0900

    added libreoffice.

---
+++ libreoffice/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿#try {
+  $downUrl = 'http://download.documentfoundation.org/libreoffice/stable/3.6.1/win/x86/LibO_3.6.1_Win_x86_install_multi.msi'
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage 'libreoffice' 'MSI' '/passive' "$downUrl" -validExitCodes @(0)
+  # the following is all part of error handling
+  #Write-ChocolateySuccess 'libreoffice'
+#} catch {
+  #Write-ChocolateyFailure 'libreoffice' "$($_.Exception.Message)"
+  #throw 
+#}
