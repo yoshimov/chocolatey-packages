2b8832250f7afe1e7e02e54a923b94f45f272c3a
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-25 18:07:29 +0900

    added audacity package.

---
+++ audacity/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿#try {
+  $downUrl = 'http://audacity.googlecode.com/files/audacity-win-2.0.2.exe'
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage 'audacity' 'EXE' '/SILENT' "$downUrl" -validExitCodes @(0)
+
+  # the following is all part of error handling
+  #Write-ChocolateySuccess 'audacity'
+#} catch {
+  #Write-ChocolateyFailure 'audacity' "$($_.Exception.Message)"
+  #throw 
+#}
