7d77df90d09a1631dcced67cb2e5e0fa98b0a041
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-25 12:05:53 +0900

    added orbit package.

---
+++ orbit/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿#try {
+  $downUrl = 'http://files.brothersoft.com/internet/download_managers/OrbitDownloaderSetup.exe'
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage 'orbit' 'EXE' '/SILENT' "$downUrl" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  #Write-ChocolateySuccess 'orbit'
+#} catch {
+  #Write-ChocolateyFailure 'orbit' "$($_.Exception.Message)"
+  #throw 
+#}
