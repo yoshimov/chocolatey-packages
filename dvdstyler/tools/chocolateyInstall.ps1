d9bdd6c3dab494993c0aa3fe97cb1ea7ee0c5b87
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-25 13:17:27 +0900

    added dvdstyler and ignore file for mediacoder.

---
+++ dvdstyler/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿#try {
+  $downUrl = 'http://downloads.sourceforge.net/project/dvdstyler/dvdstyler/2.3/DVDStyler-2.3-win32.exe?r=http%3A%2F%2Fwww.dvdstyler.org%2Fen%2Fdownloads&ts=1348546019&use_mirror=auto'
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage 'dvdstyler' 'EXE' '/SILENT' "$downUrl" -validExitCodes @(0)
+
+  # the following is all part of error handling
+  #Write-ChocolateySuccess 'dvdstyler'
+#} catch {
+  #Write-ChocolateyFailure 'dvdstyler' "$($_.Exception.Message)"
+  #throw 
+#}
