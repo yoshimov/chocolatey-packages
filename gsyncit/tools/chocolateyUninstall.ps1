dfbedf3e440b4d1db9ab183530f9b15db8d309a4
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-27 16:27:42 +0900

    added uninstaller for gsyncit.

---
+++ gsyncit/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿#try {
+  $msid = '{77B5D56B-3339-4171-B033-673DEF7A684C}'
+  Uninstall-ChocolateyPackage 'gsyncit' 'MSI' "$msid" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  #Write-ChocolateySuccess 'gsyncit'
+#} catch {
+  #Write-ChocolateyFailure 'gsyncit' "$($_.Exception.Message)"
+  #throw 
+#}
