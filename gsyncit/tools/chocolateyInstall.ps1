9c754bcd7ec6db6d3e5bbfb3fdd28b98da2d82ed
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-09-27 12:33:53 +0900

    updated gsyncit version.

--- gsyncit/tools/chocolateyInstall.ps1
+++ gsyncit/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿#try {
-  $downUrl = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit_3_6_37.msi'
-  $down64Url = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit64_3_6_37.msi'
+  $downUrl = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit_3_6_41.msi'
+  $down64Url = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit64_3_6_41.msi'
   Install-ChocolateyPackage 'gsyncit' 'MSI' '/passive' "$downUrl" "$down64Url" -validExitCodes @(0)
   
   # the following is all part of error handling
