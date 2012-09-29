c8535ead6a82b2c111b9156eb40ec46312db5110
NOMURA Yoshihide <nomura@pobox.com>
2012-09-30 02:10:19 +0900

    added mobalivecd package.

---
+++ mobalivecd/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+  $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $downUrl = 'http://mobalivecd.mobatek.net/en/MobaLiveCD_v2.1.exe'
+  #Install-ChocolateyPackage 'mobalivecd' 'EXE' '/SILENT' "$downUrl" -validExitCodes @(0)
+  Get-ChocolateyWebFile 'mobalivecd' "${toolsDir}\MobaLiveCD_v2.1.exe" "$downUrl"
+  Install-ChocolateyDesktopLink "${toolsDir}\MobaLiveCD_v2.1.exe"
+
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'mobalivecd'
+} catch {
+  Write-ChocolateyFailure 'mobalivecd' "$($_.Exception.Message)"
+  throw 
+}
