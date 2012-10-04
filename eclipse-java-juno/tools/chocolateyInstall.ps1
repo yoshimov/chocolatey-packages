d3c51a41b31322d93c0df06b6875265b9b628a71
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-10-04 18:06:59 +0900

    updated eclipse as 4.2.1

--- eclipse-java-juno/tools/chocolateyInstall.ps1
+++ eclipse-java-juno/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  $package = 'eclipse-juno'
+﻿try {
+  $package = 'eclipse-juno-4.2.1'
 
   $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
   ### For BinRoot, use the following instead ###
@@ -7,13 +7,16 @@
   ### Using an environment variable to to define the bin root until we implement configuration ###
   if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
   $installDir = Join-Path $binRoot $package
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  $zipUrl = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/R/eclipse-java-juno-win32.zip&r=1'
-  $zip64Url = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/R/eclipse-java-juno-win32-x86_64.zip&r=1'
+  $zipUrl = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/SR1/eclipse-java-juno-SR1-win32.zip&r=1'
+  $zip64Url = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/juno/SR1/eclipse-java-juno-SR1-win32-x86_64.zip&r=1'
 
   Install-ChocolateyZipPackage 'eclipse-java-juno' "$zipUrl" "$binRoot" "$zip64Url"
   
-  Rename-Item "$binRoot\eclipse" "$package" -Force
+  if (Test-Path "$installDir") {
+    $today = $(Get-Date -Format "yyyyMMdd")
+    Move-Item "$installDir" "${installDir}.${today}"
+  }
+  Rename-Item "${binRoot}\eclipse" "$package" -Force
 
   $target = Join-Path $installDir 'eclipse.exe'
   Install-ChocolateyDesktopLink $target
---
+++ eclipse-java-juno/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+  $package = 'eclipse-juno-4.2.1'
+
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Remove-Item -Force -Recurse $installDir
+
+  Write-ChocolateySuccess 'eclipse-java-juno'
+} catch {
+  Write-ChocolateyFailure 'eclipse-java-juno' "$($_.Exception.Message)"
+  #throw 
+}
