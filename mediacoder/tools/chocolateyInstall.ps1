8e6782fec487b46f6a7ac188dcce1e367ede58b5
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-10-16 16:24:54 +0900

    updated mediacoder version.

--- mediacoder/tools/chocolateyInstall.ps1
+++ mediacoder/tools/chocolateyInstall.ps1
@@ -1,14 +1,10 @@
 ﻿try {
   # installer, will assert administrative rights
-  $downUrl = 'http://mc.rengaosh.com/dl/_files/MediaCoder-0.8.15.5280.zip'
-  $down64Url = 'http://mc.rengaosh.com/dl/_files/MediaCoder-x64-0.8.15.5280.zip'
+  $downUrl = 'http://dl.mediacoderhq.com/dl/_files/MediaCoder-0.8.16.5292.exe'
+  $down64Url = 'http://dl.mediacoderhq.com/dl/_files/MediaCoder-x64-0.8.16.5292.exe'
   $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  Install-ChocolateyZipPackage 'mediacoder' "$downUrl" "$toolsDir" "$down64Url" 
+  Install-ChocolateyPackage 'mediacoder' 'EXE' '/S' "$downUrl" "$down64Url" -validExitCodes @(0)
   
-  Rename-Item "$toolsDir\MediaCoder-0.8.15.5280.exe" "mediacoder.exe" -Force
-  Rename-Item "$toolsDir\MediaCoder-x64-0.8.15.5280.exe" "mediacoder.exe" -Force
-  Start-ChocolateyProcessAsAdmin "$toolsDir\mediacoder.exe" -validExitCodes @(0)
-
   Write-ChocolateySuccess 'mediacoder'
 } catch {
   Write-ChocolateyFailure 'mediacoder' "$($_.Exception.Message)"
