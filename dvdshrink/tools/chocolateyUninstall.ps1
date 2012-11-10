c4210f56535a09cfa519b1adec4b5df08ac809f7
NOMURA Yoshihide <nomura@pobox.com>
2012-11-11 07:35:19 +0900

    added dvdshrink.

---
+++ dvdshrink/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+  if ($is64bit) {
+    $unpath = "${Env:ProgramFiles(x86)}\DVD Shrink\unins000.exe"
+  } else {
+    $unpath = "${Env:ProgramFiles}\DVD Shrink\unins000.exe"
+  }
+  Uninstall-ChocolateyPackage 'dvdshrink' 'EXE' '/SILENT' "$unpath" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'dvdshrink'
+} catch {
+  Write-ChocolateyFailure 'dvdshrink' "$($_.Exception.Message)"
+  throw 
+}
