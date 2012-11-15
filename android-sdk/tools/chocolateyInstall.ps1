228ffefbf57fb66df4b7d455e731e4a2ced52152
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-11-15 12:24:21 +0900

    updated android-sdk to 21

--- android-sdk/tools/chocolateyInstall.ps1
+++ android-sdk/tools/chocolateyInstall.ps1
@@ -1,5 +1,7 @@
 ﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  Install-ChocolateyPackage 'android-sdk' 'EXE' '/S' 'http://dl.google.com/android/installer_r20.0.3-windows.exe' -validExitCodes @(0)
+  $downUrl = 'http://dl.google.com/android/installer_r21-windows.exe'
+  $md5hash = '29ca8cb8f0bc8db627fa2adc2139a3cc'
+  Install-ChocolateyPackage 'android-sdk' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
 
   #Write-ChocolateySuccess 'android-sdk'
 #} catch {
---
+++ android-sdk/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿try {
+  $unpath = "${Env:LOCALAPPDATA}\Android\android-sdk\uninstall.exe"
+  Uninstall-ChocolateyPackage 'android-sdk' 'EXE' '/S' "$unpath"
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'android-sdk'
+} catch {
+  Write-ChocolateyFailure 'android-sdk' "$($_.Exception.Message)"
+  throw 
+}
