6c541910b5dcb686c27cec59d28591502f8f16c6
NOMURA Yoshihide <nomura@pobox.com>
2012-11-29 05:43:54 +0900

    added freefilesync.

---
+++ freefilesync/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿try {
+  $downUrl = 'http://downloads.sourceforge.net/project/freefilesync/freefilesync/v5.9/FreeFileSync_5.9_setup.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ffreefilesync%2F%3Fsource%3Ddlp&use_mirror=auto'
+  Install-ChocolateyPackage 'freefilesync' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'freefilesync'
+} catch {
+  Write-ChocolateyFailure 'freefilesync' "$($_.Exception.Message)"
+  throw 
+}
---
+++ freefilesync/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿try {
+  $unpath = "${Env:ProgramFiles}\FreeFileSync\uninstall.exe"
+  Uninstall-ChocolateyPackage 'freefilesync' 'EXE' '/S' "$unpath" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'freefilesync'
+} catch {
+  Write-ChocolateyFailure 'freefilesync' "$($_.Exception.Message)"
+  throw 
+}
