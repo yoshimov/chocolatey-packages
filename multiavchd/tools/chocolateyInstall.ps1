120a923f2991939d6b0cd8410cd5d86b412ef186
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2012-11-29 13:27:54 +0900

    added multiavchd.

---
+++ multiavchd/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿try {
+  $baseUrl = "http://www.sendspace.com/file/zajv0n"
+  [void]([Reflection.Assembly]::LoadWithPartialName("System.Web"))
+  $webReq = [Net.HttpWebRequest]::Create("$baseUrl")
+  $webReq.Method = "GET"
+
+  $webRes = $webReq.GetResponse()
+  $sr = New-Object IO.StreamReader($webRes.GetResponseStream(), $webRes.ContentEncoding)
+  $content = $sr.ReadToEnd()
+  $sr.Close()
+  $webRes.Close()
+
+  #echo $content
+  if($content -match 'id="download_button" href="(http:[\/\.0-9a-zA-Z_]+)"')
+  {
+     $downUrl = $matches[1]
+#     echo "$($matches[1])"
+  }
+
+  Install-ChocolateyPackage 'multiavchd' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+
+  Write-ChocolateySuccess 'multiavchd'
+} catch {
+  Write-ChocolateyFailure 'multiavchd' "$($_.Exception.Message)"
+  throw 
+}
---
+++ multiavchd/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿try {
+  $unpath = "${Env:SystemDrive}\multiAVCHD\uninstall.exe"
+  Uninstall-ChocolateyPackage 'multiavchd' 'EXE' '/S' "$unpath" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'multiavchd'
+} catch {
+  Write-ChocolateyFailure 'multiavchd' "$($_.Exception.Message)"
+  throw 
+}
