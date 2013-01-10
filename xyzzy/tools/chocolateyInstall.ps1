30e6a59b2207ea8b47dcbb347745f63361356e33
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2013-01-10 18:48:41 +0900

    updated xyzzy package.

--- xyzzy/tools/chocolateyInstall.ps1
+++ xyzzy/tools/chocolateyInstall.ps1
@@ -1,14 +1,15 @@
 ﻿try {
+  $downUrl = "http://xyzzy-022.github.com/downloads/xyzzy-0.2.2.246.zip"
   $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
-  Get-ChocolateyWebFile 'xyzzy' "$parentpath\xyzzy.lzh" 'http://www.jsdlab.co.jp/~kamei/cgi-bin/download.cgi'
-  cmd /c "$parentpath\tools\extractlzh.bat"
-#  Start-ChocolateyProcessAsAdmin "cd $parentpath; tools\lha32.exe x xyzzy.lzh" -validExitCodes @(0)
+  Get-ChocolateyWebFile 'xyzzy' "${parentpath}\xyzzy.zip" "$downUrl"
+  $cmd7z = "${Env:ProgramFiles}\7-Zip\7z.exe"
+  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$parentpath" -ArgumentList "x ${parentpath}\xyzzy.zip"
   Install-ChocolateyPath "$parentpath\xyzzy" 'User' # Machine will assert administrative rights
 
   $target = Join-Path $parentpath 'xyzzy\xyzzy.exe'
   Install-ChocolateyDesktopLink $target
   
-  #Write-ChocolateySuccess 'xyzzy'
+  Write-ChocolateySuccess 'xyzzy'
 } catch {
   Write-ChocolateyFailure 'xyzzy' "$($_.Exception.Message)"
   throw 
--- xyzzy/tools/extractlzh.bat
+++
@@ -1,2 +0,0 @@
-cd /d "%~dp0"\..
-"%ProgramFiles%\7-zip\7z.exe" x xyzzy.lzh
