e9025aea5b88034e3667b6df2c2f189c2b12bb78
Christine Emrich <christine.emrich@web.de>
2013-01-14 23:05:41 +0100

    Updated the libreoffice package from version 3.6.3 to 3.6.4

--- libreoffice/tools/chocolateyUninstall.ps1
+++ libreoffice/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿try {
   # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-  $msid = '{CBCF6C86-4738-4A84-9C2C-331804DCEB9B}'
+  $msid = '{60B2F25C-22CB-4CD9-9168-8C63708DC1A1}'
   Uninstall-ChocolateyPackage 'libreoffice' 'MSI' "$msid" -validExitCodes @(0)
   
   # the following is all part of error handling
