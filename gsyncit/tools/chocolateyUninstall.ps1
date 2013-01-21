315714859a51ba395555f8c15246428cf53c016d
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2013-01-21 10:31:04 +0900

    updated gsyncit as 3.6.111

--- gsyncit/tools/chocolateyUninstall.ps1
+++ gsyncit/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿#try {
-  # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{D682D879-C8F2-4A22-BEA6-07502F82FD56}
-  $msid = '{D682D879-C8F2-4A22-BEA6-07502F82FD56}'
+  # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{42226108-1887-4B4B-A8A4-D985D8679493}
+  $msid = '{42226108-1887-4B4B-A8A4-D985D8679493}'
   Uninstall-ChocolateyPackage 'gsyncit' 'MSI' "$msid" -validExitCodes @(0)
   
   # the following is all part of error handling
