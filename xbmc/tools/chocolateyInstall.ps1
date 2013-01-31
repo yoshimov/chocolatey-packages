68f29aeee6d55d0f201169d07e86377120668a4a
NOMURA Yoshihide <nomura@pobox.com>
2013-02-01 07:49:55 +0900

    initial version of xbmc

---
+++ xbmc/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿try {
+  # installer, will assert administrative rights
+  $downUrl = 'http://mirror.optusnet.com.au/xbmc/releases/win32/xbmc-12.0.exe'
+  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Install-ChocolateyPackage 'mediacoder' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+  
+  Write-ChocolateySuccess 'mediacoder'
+} catch {
+  Write-ChocolateyFailure 'mediacoder' "$($_.Exception.Message)"
+  throw 
+}
