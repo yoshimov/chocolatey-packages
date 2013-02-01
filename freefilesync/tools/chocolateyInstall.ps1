25473c016d3530444080c4018e68fb8c3cf33abb
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2013-02-01 17:24:58 +0900

    updated freefilesync as 5.11

--- freefilesync/tools/chocolateyInstall.ps1
+++ freefilesync/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿try {
-  $downUrl = 'http://downloads.sourceforge.net/project/freefilesync/freefilesync/v5.9/FreeFileSync_5.9_setup.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ffreefilesync%2F%3Fsource%3Ddlp&use_mirror=auto'
+  $downUrl = 'http://downloads.sourceforge.net/project/freefilesync/freefilesync/v5.11/FreeFileSync_5.11_setup.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ffreefilesync%2F%3Fsource%3Ddlp&use_mirror=auto'
   Install-ChocolateyPackage 'freefilesync' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
   
   # the following is all part of error handling
