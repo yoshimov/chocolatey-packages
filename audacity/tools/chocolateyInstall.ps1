47a7d40e838db9222db05a574bba8857ab77ecf4
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2013-02-19 19:10:24 +0900

    updated audacity as 2.0.3

--- audacity/tools/chocolateyInstall.ps1
+++ audacity/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿#try {
-  $downUrl = 'http://audacity.googlecode.com/files/audacity-win-2.0.2.exe'
+﻿try {
+  $downUrl = 'http://audacity.googlecode.com/files/audacity-win-2.0.3.exe'
   # installer, will assert administrative rights
   Install-ChocolateyPackage 'audacity' 'EXE' '/SILENT' "$downUrl" -validExitCodes @(0)
 
   # the following is all part of error handling
-  #Write-ChocolateySuccess 'audacity'
-#} catch {
-  #Write-ChocolateyFailure 'audacity' "$($_.Exception.Message)"
-  #throw 
-#}
+  Write-ChocolateySuccess 'audacity'
+} catch {
+  Write-ChocolateyFailure 'audacity' "$($_.Exception.Message)"
+  throw 
+}
