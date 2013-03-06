e6e10bee34e66257ecc47c8097b604e3b634e184
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2013-03-06 11:18:04 +0900

    updated gsyncit to 3.6.128

--- gsyncit/tools/chocolateyInstall.ps1
+++ gsyncit/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿#try {
-  $downUrl = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit_3_6_111.msi'
-  $down64Url = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit64_3_6_111.msi'
+﻿try {
+  $downUrl = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit_3_6_128.msi'
+  $down64Url = 'http://www.fieldstonsoftware.com/software/gsyncit3/downloads/gSyncit64_3_6_128.msi'
   Install-ChocolateyPackage 'gsyncit' 'MSI' '/passive' "$downUrl" "$down64Url" -validExitCodes @(0)
   
   # the following is all part of error handling
-  #Write-ChocolateySuccess 'gsyncit'
-#} catch {
-  #Write-ChocolateyFailure 'gsyncit' "$($_.Exception.Message)"
-  #throw 
-#}
+  Write-ChocolateySuccess 'gsyncit'
+} catch {
+  Write-ChocolateyFailure 'gsyncit' "$($_.Exception.Message)"
+  throw 
+}
--- gsyncit/tools/chocolateyUninstall.ps1
+++ gsyncit/tools/chocolateyUninstall.ps1
@@ -1,11 +1,11 @@
-﻿#try {
-  # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{42226108-1887-4B4B-A8A4-D985D8679493}
-  $msid = '{42226108-1887-4B4B-A8A4-D985D8679493}'
+﻿try {
+  # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{B91569AF-DED4-467F-ADF0-A02501839CD0}
+  $msid = '{B91569AF-DED4-467F-ADF0-A02501839CD0}'
   Uninstall-ChocolateyPackage 'gsyncit' 'MSI' "$msid" -validExitCodes @(0)
   
   # the following is all part of error handling
-  #Write-ChocolateySuccess 'gsyncit'
-#} catch {
-  #Write-ChocolateyFailure 'gsyncit' "$($_.Exception.Message)"
-  #throw 
-#}
+  Write-ChocolateySuccess 'gsyncit'
+} catch {
+  Write-ChocolateyFailure 'gsyncit' "$($_.Exception.Message)"
+  throw 
+}
