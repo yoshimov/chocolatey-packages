46392ef753b4ba6d6d8c61af05bbee6d14b9d83c
NOMURA Yoshihide <nomura@pobox.com>
2013-05-01 04:28:05 +0900

    added cevio creative studio.

---
+++ cevio/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿try {
+  $pkgid='cevio'
+  $clsid='{0CF8F7F2-7801-482F-9AB5-F3800F410972}'
+  if (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$clsid") {
+    Write-ChocolateyFailure "$pkgid" "CeVIO Creative Studio is already installed!"
+  }
+  else {
+    $downUrl = 'http://cevio.jp/download/CeVIO_Creative_Studio_FREE_Setup_(1.0.0.1).msi'
+    # installer, will assert administrative rights
+    Install-ChocolateyPackage "$pkgid" 'MSI' '/passive' "$downUrl" -validExitCodes @(0)
+    # the following is all part of error handling
+    Write-ChocolateySuccess "$pkgid"
+  }
+} catch {
+  Write-ChocolateyFailure "$pkgid" "$($_.Exception.Message)"
+  throw 
+}
