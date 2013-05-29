103b0e68b2467cd80b5188821614d56ca176a52c
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2013-05-29 18:10:06 +0900

    changed to use get-webfile instead of webrequest for proxy.

--- astah/tools/chocolateyInstall.ps1
+++ astah/tools/chocolateyInstall.ps1
@@ -9,14 +9,7 @@
   } else {
     $url = 'http://members.change-vision.com/files/astah_community/6_6_4/astah-community-6_6_4-41775-jre-setup.exe'
   }
-  $webReq = [Net.HttpWebRequest]::Create("$url")
-  $webReq.Method = "GET"
-
-  $webRes = $webReq.GetResponse()
-  $sr = New-Object IO.StreamReader($webRes.GetResponseStream(), $webRes.ContentEncoding)
-  $content = $sr.ReadToEnd()
-  $sr.Close()
-  $webRes.Close()
+  $content = Get-WebFile "$url" -Passthru
 
   if($content -match '<a href="(/files/([0-9A-Za-z_\-]+)/astah_community/.+exe;jsessionid=[0-9A-Z]+)"')
   {
