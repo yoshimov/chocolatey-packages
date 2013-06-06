309e434f76d0127f051adb4011e0eb5e3227e0db
Justin Dearing <zippy1981@gmail.com>
2013-06-06 06:47:57 -0400

    Bumped R.Project to 3.0.1

--- r-project/tools/chocolateyInstall.ps1
+++ r-project/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
-﻿$url='http://cran.at.r-project.org/bin/windows/base/R-3.0.0-win.exe'
+﻿$url='http://cran.at.r-project.org/bin/windows/base/R-3.0.1-win.exe'
 Install-ChocolateyPackage 'R.Project' 'exe' '/silent' "$url" -validExitCodes @(0)
