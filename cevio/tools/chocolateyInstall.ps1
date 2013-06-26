0e2c3d26bc62a78a56d55708deb829a25b2b9799
NOMURA Yoshihide <y.nomura@jp.fujitsu.com>
2013-06-26 16:40:47 +0900

    updated cevio to 1.2.4.2

--- cevio/tools/chocolateyInstall.ps1
+++ cevio/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿try {
   $pkgid='cevio'
-  $clsid='{0CF8F7F2-7801-482F-9AB5-F3800F410972}'
+  $clsid='{7B13DAA4-2DE5-4D26-B9FB-1C5FAB19E662}'
   if (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$clsid") {
     Write-ChocolateyFailure "$pkgid" "CeVIO Creative Studio is already installed!"
   }
   else {
-    $downUrl = 'http://cevio.jp/download/CeVIO_Creative_Studio_FREE_Setup_(1.0.0.1).msi'
+    $downUrl = 'http://cevio.jp/download/CeVIO_Creative_Studio_FREE_Setup_(1.2.4.2).msi'
     # installer, will assert administrative rights
     Install-ChocolateyPackage "$pkgid" 'MSI' '/passive' "$downUrl" -validExitCodes @(0)
     # the following is all part of error handling
--- cevio/tools/chocolateyUninstall.ps1
+++ cevio/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿try {
   $pkgid='cevio'
-  $clsid='{0CF8F7F2-7801-482F-9AB5-F3800F410972}'
+  $clsid='{7B13DAA4-2DE5-4D26-B9FB-1C5FAB19E662}'
   Uninstall-ChocolateyPackage "$pkgid" 'MSI' "$clsid" -validExitCodes @(0)
   
   # the following is all part of error handling
