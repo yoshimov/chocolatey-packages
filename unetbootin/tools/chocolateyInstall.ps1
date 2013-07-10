66b95c099ff54a4f3340c5a89184552a159b728e
TomOne <user.email@fake.com>
2013-07-10 12:34:49 +0200

    bumped unetbootin to v5.8.5

--- unetbootin/tools/chocolateyInstall.ps1
+++ unetbootin/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿try {
   $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $downUrl = 'http://sourceforge.net/projects/unetbootin/files/UNetbootin/584/unetbootin-windows-584.exe/download'
+  $downUrl = 'http://sourceforge.net/projects/unetbootin/files/UNetbootin/585/unetbootin-windows-585.exe/download'
   Get-ChocolateyWebFile 'unetbootin' "${toolsDir}\Unetbootin.exe" "$downUrl"
   
   # the following is all part of error handling
