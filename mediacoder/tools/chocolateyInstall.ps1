22ea7874f7ff5cd650fd221bb0c6494a7d53e484
NOMURA Yoshihide <nomura@pobox.com>
2013-08-16 03:15:25 +0900

    updated mediacoder to 0.8.25

--- mediacoder/tools/chocolateyInstall.ps1
+++ mediacoder/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿try {
   # installer, will assert administrative rights
-  $downUrl = 'http://dl.mediacoderhq.com/files001/MediaCoder-0.8.20.5380.exe'
-  $down64Url = 'http://dl.mediacoderhq.com/files001/MediaCoder-x64-0.8.20.5380.exe'
+  $downUrl = 'http://dl.mediacoderhq.com/files001/MediaCoder-0.8.25.5550.exe'
+  $down64Url = 'http://dl.mediacoderhq.com/files001/MediaCoder-x64-0.8.25.5550.exe'
 #  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Install-ChocolateyPackage 'mediacoder' 'EXE' '/S' "$downUrl" "$down64Url" -validExitCodes @(0)
   
