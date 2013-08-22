0e33ead63414b720944820441d95677dd4aee1ce
NOMURA Yoshihide <nomura@pobox.com>
2013-08-23 01:58:12 +0900

    added eclipse kepler.

---
+++ eclipse-standard-kepler/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿try {
+  $cname="eclipse-standard-kepler"
+  $package = 'eclipse-kepler-4.3'
+
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  $zipUrl = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/kepler/R/eclipse-standard-kepler-R-win32.zip&r=1'
+  $zip64Url = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/kepler/R/eclipse-standard-kepler-R-win32-x86_64.zip&r=1'
+
+  Install-ChocolateyZipPackage "$cname" "$zipUrl" "$binRoot" "$zip64Url"
+  
+  if (Test-Path "$installDir") {
+    $today = $(Get-Date -Format "yyyyMMdd")
+    Move-Item "$installDir" "${installDir}.${today}"
+  }
+  Rename-Item "${binRoot}\eclipse" "$package" -Force
+
+  $target = Join-Path $installDir 'eclipse.exe'
+  Install-ChocolateyDesktopLink $target
+
+  Write-ChocolateySuccess "$cname"
+} catch {
+  Write-ChocolateyFailure "$cname" "$($_.Exception.Message)"
+  #throw 
+}
---
+++ eclipse-standard-kepler/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿try {
+  $cname = "eclipse-standard-kepler"
+  $package = 'eclipse-kepler-4.3'
+
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Remove-Item -Force -Recurse $installDir
+
+  Write-ChocolateySuccess "$name"
+} catch {
+  Write-ChocolateyFailure "$name" "$($_.Exception.Message)"
+  #throw 
+}
