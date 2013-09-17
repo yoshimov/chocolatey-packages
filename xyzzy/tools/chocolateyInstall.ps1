d6c73f0d0bc596771f1cbfcc7c51bae5f26c7b62
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2013-09-17 16:43:55 +0900

    updated xyzzy to 0.2.2.251

--- xyzzy/tools/chocolateyInstall.ps1
+++ xyzzy/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
-﻿try {
-  $downUrl = "http://xyzzy-022.github.com/downloads/xyzzy-0.2.2.246.zip"
+﻿#try {
+  $downUrl = "http://xyzzy-022.github.com/downloads/xyzzy-0.2.2.251.zip"
   $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
   Get-ChocolateyWebFile 'xyzzy' "${parentpath}\xyzzy.zip" "$downUrl"
   $cmd7z = "${Env:ProgramFiles}\7-Zip\7z.exe"
-  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$parentpath" -ArgumentList "x ${parentpath}\xyzzy.zip"
+  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$parentpath" -ArgumentList "x -y ${parentpath}\xyzzy.zip"
   Install-ChocolateyPath "$parentpath\xyzzy" 'User' # Machine will assert administrative rights
 
   $target = Join-Path $parentpath 'xyzzy\xyzzy.exe'
   Install-ChocolateyDesktopLink $target
   
   Write-ChocolateySuccess 'xyzzy'
-} catch {
-  Write-ChocolateyFailure 'xyzzy' "$($_.Exception.Message)"
-  throw 
-}
+#} catch {
+#  Write-ChocolateyFailure 'xyzzy' "$($_.Exception.Message)"
+#  throw 
+#}
