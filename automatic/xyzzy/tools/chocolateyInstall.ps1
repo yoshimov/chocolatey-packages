f33982eee3f795904ce9902bfdcd984fff0b107e
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2013-09-19 13:00:07 +0900

    changed xyzzy as automated package.

---
+++ automatic/xyzzy/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$cname = "{{PackageName}}"
+#try {
+  $downUrl = "{{DownloadUrl}}"
+  $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
+  Get-ChocolateyWebFile $cname "${parentpath}\xyzzy.zip" "$downUrl"
+  $cmd7z = "${Env:ProgramFiles}\7-Zip\7z.exe"
+  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$parentpath" -ArgumentList "x -y ${parentpath}\xyzzy.zip"
+  Install-ChocolateyPath "$parentpath\xyzzy" 'User' # Machine will assert administrative rights
+
+  $target = Join-Path $parentpath 'xyzzy\xyzzy.exe'
+  Install-ChocolateyDesktopLink $target
+  
+  Write-ChocolateySuccess $cname
+#} catch {
+#  Write-ChocolateyFailure $cname "$($_.Exception.Message)"
+#  throw 
+#}
