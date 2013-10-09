03a1f2137108c9a799416a4abc61159072c35caa
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2013-10-09 13:39:13 +0900

    added open garden. but its uninstaller is not working correctly.

---
+++ automatic/opengarden/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$cname = '{{PackageName}}'
+$ver = '{{PackageVersion}}'
+
+#try {
+  # installer, will assert administrative rights
+  $downUrl = '{{DownloadUrl}}'
+  Install-ChocolateyPackage $cname 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+  
+  Write-ChocolateySuccess $cname
+#} catch {
+#  Write-ChocolateyFailure $cname "$($_.Exception.Message)"
+#  throw 
+#}
---
+++ automatic/opengarden/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$cname = '{{PackageName}}'
+$ver = '{{PackageVersion}}'
+
+#try {
+  $unpath = "${Env:APPDATA}\Open Garden\OpenGarden.exe"
+  Uninstall-ChocolateyPackage $cname 'EXE' '/uninstall2' "$unpath" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess $cname
+#} catch {
+#  Write-ChocolateyFailure $cname "$($_.Exception.Message)"
+#  throw 
+#}
