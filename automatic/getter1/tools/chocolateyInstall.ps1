4b41e5e42ce9051eab00c4aac696c912d5c75584
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2013-10-18 13:36:55 +0900

    changed getter1 as automated package.

---
+++ automatic/getter1/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$cname = '{{PackageName}}'
+#try {
+  $downUrl = '{{DownloadUrl}}'
+  $toolsDir = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
+  Install-ChocolateyZipPackage $cname $downUrl $toolsDir
+  Write-ChocolateySuccess $cname
+#} catch {
+#  Write-ChocolateyFailure 'hhkbcng' "$($_.Exception.Message)"
+#  throw 
+#}
