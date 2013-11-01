6711164d689e0042e50abe830628efe573e93be0
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2013-11-01 10:39:59 +0900

    added gitblit as automated package.

---
+++ automatic/gitblit/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$cname = "{{PackageName}}"
+#try {
+  $downUrl = "{{DownloadUrl}}"
+  $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
+  $binroot = "${Env:SystemDrive}\${Env:chocolatey_bin_root}"
+  $installdir = "${binroot}\gitblit"
+  $cmd7z = "${Env:ProgramFiles}\7-Zip\7z.exe"
+  New-Item -ItemType dir -Force "$installdir"
+  Get-ChocolateyWebFile $cname "${parentpath}\gitblit.zip" "$downUrl"
+  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$installdir" -ArgumentList "x -y ${parentpath}\gitblit.zip"
+  Install-ChocolateyPath "${installdir}\gitblit" 'User'
+
+  Write-ChocolateySuccess $cname
+#} catch {
+#  Write-ChocolateyFailure $cname "$($_.Exception.Message)"
+#  throw 
+#}
