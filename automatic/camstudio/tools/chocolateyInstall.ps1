bda306c1d353829a130b81373d75d385225c79db
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2014-02-18 17:21:22 +0900

    updated camstudio.

--- automatic/camstudio/tools/chocolateyInstall.ps1
+++ automatic/camstudio/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
 $installerType = 'exe'
 $installArgs = '/SILENT'
 # {{DownloadUrlx64}} gets “misused” as filename for the installer here.
-$url = 'http://sourceforge.net/projects/camstudio/files/stable/{{DownloadUrlx64}}/download'
+$url = '{{DownloadUrl}}'
 
 Install-ChocolateyPackage $packageName $installerType $installArgs $url -validExitCodes @(0)
