cb4df1ce883f9518b9d22ac96184219fd4bb7ba4
NOMURA Yoshihide <nomura@pobox.com>
2014-09-09 05:15:09 +0900

    updated truecrypt, freefilesync, libreoffice, mediacoder and xbmc.

--- automatic/freefilesync/tools/chocolateyInstall.ps1
+++ automatic/freefilesync/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-﻿try {
-  $downUrl = 'http://sourceforge.net/projects/freefilesync/files/freefilesync/v{{PackageVersion}}/FreeFileSync_{{PackageVersion}}_Windows_Setup.exe/download'
+﻿#try {
+  $downUrl = '{{DownloadUrl}}'
   Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
   
   # the following is all part of error handling
-  Write-ChocolateySuccess '{{PackageName}}'
-} catch {
-  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
-  throw 
-}
+#  Write-ChocolateySuccess '{{PackageName}}'
+#} catch {
+#  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
+#  throw 
+#}
--- truecrypt/tools/chocolateyInstall.ps1
+++ truecrypt/tools/chocolateyInstall.ps1
@@ -1,30 +1,11 @@
-﻿try {
-  # obtain download id from web
-  [void]([Reflection.Assembly]::LoadWithPartialName("System.Web"))
-  $url = 'http://www.truecrypt.org/downloads'
-  $webReq = [Net.HttpWebRequest]::Create("$url")
-  $webReq.Method = "GET"
-  
-  $webRes = $webReq.GetResponse()
-  $sr = New-Object IO.StreamReader($webRes.GetResponseStream(), $webRes.ContentEncoding)
-  $content = $sr.ReadToEnd()
-  $sr.Close()
-  $webRes.Close()
-
-  if($content -match 'name="LinkT" value="([0-9a-z]+)">')
-  {
-     $did = "$($matches[1])"
-     Echo "download id = $did"
-  }
-
-  $downUrl = "http://www.truecrypt.org/download/transient/${did}/TrueCrypt%20Setup%207.1a.exe"
-  Echo "download url = $downUrl"
+﻿#try {
+  $downUrl = "https://download.truecrypt.ch/current/TrueCrypt%20Setup%207.1a.exe"
   # installer, will assert administrative rights
   Install-ChocolateyPackage 'truecrypt' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
   
   # the following is all part of error handling
-  Write-ChocolateySuccess 'truecrypt'
-} catch {
-  Write-ChocolateyFailure 'truecrypt' "$($_.Exception.Message)"
-  throw 
-}
+#  Write-ChocolateySuccess 'truecrypt'
+#} catch {
+#  Write-ChocolateyFailure 'truecrypt' "$($_.Exception.Message)"
+#  throw 
+#}
