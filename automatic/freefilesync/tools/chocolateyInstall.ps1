b4ae0829f2b0e20381d891c504093d81c190da0d
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2014-09-09 10:09:13 +0900

    fixed freefilesync download url.

--- automatic/freefilesync/tools/chocolateyInstall.ps1
+++ automatic/freefilesync/tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 ﻿#try {
-  $downUrl = '{{DownloadUrl}}'
+#  $downUrl = '{{DownloadUrl}}'
+  $downUrl = 'http://sourceforge.net/projects/freefilesync/files/FreeFileSync/{{PackageVersion}}/FreeFileSync_{{PackageVersion}}_Windows_Setup.exe/download'
   Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
   
   # the following is all part of error handling
