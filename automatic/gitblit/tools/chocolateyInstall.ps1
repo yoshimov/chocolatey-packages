78710913a45bfd7d306fe16df6ee3a176312b253
NOMURA Yoshihide <nomura@pobox.com>
2014-11-06 03:51:10 +0900

    changed to use install command in scala. changed to use get binroot command in gitblit.

--- automatic/gitblit/tools/chocolateyInstall.ps1
+++ automatic/gitblit/tools/chocolateyInstall.ps1
@@ -1,17 +1,5 @@
 ﻿$cname = "{{PackageName}}"
-#try {
-  $downUrl = "{{DownloadUrl}}"
-  $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
-  $binroot = "${Env:SystemDrive}\${Env:chocolatey_bin_root}"
-  $installdir = "${binroot}\gitblit"
-  $cmd7z = "${Env:ProgramFiles}\7-Zip\7z.exe"
-  New-Item -ItemType dir -Force "$installdir"
-  Get-ChocolateyWebFile $cname "${parentpath}\gitblit.zip" "$downUrl"
-  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$installdir" -ArgumentList "x -y ${parentpath}\gitblit.zip"
-  Install-ChocolateyPath "${installdir}\gitblit" 'User'
-
-  Write-ChocolateySuccess $cname
-#} catch {
-#  Write-ChocolateyFailure $cname "$($_.Exception.Message)"
-#  throw 
-#}
+$downUrl = "{{DownloadUrl}}"
+$binroot = Get-BinRoot
+$installdir = "${binroot}\gitblit"
+Install-ChocolateyZipPackage $cname "$downUrl" "$installdir"
--- automatic/scala.portable/tools/chocolateyInstall.ps1
+++ automatic/scala.portable/tools/chocolateyInstall.ps1
@@ -1,21 +1,11 @@
-﻿try {
-  $package = 'scala-{{PackageVersion}}'
-  $pkgid = '{{PackageName}}'
+﻿$package = 'scala-{{PackageVersion}}'
+$pkgid = '{{PackageName}}'
 
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
-  ### For BinRoot, use the following instead ###
-  $binRoot = "$env:systemdrive\"
-  ### Using an environment variable to to define the bin root until we implement configuration ###
-  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
-  $installDir = Join-Path $binRoot $package
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  $zipUrl = '{{DownloadUrl}}'
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 
-  Install-ChocolateyZipPackage $pkgid "$zipUrl" "$binRoot"
-  
-  Install-ChocolateyPath "$installDir\bin" 'User' # Machine will assert administrative rights
-  Write-ChocolateySuccess $pkgid
-} catch {
-  Write-ChocolateyFailure $pkgid "$($_.Exception.Message)"
-  throw 
-}
+$binRoot = Get-BinRoot
+$installDir = Join-Path $binRoot $package
+Write-Host "Adding `'$installDir`' to the path and the current shell path"
+$zipUrl = '{{DownloadUrl}}'
+Install-ChocolateyZipPackage $pkgid "$zipUrl" "$binRoot"
+Install-ChocolateyPath "$installDir\bin" 'User' # Machine will assert administrative rights
