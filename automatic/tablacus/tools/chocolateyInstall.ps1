a2580e2fc3c4229d1d4890a81e81da538a0f130f
NOMURA Yoshihide <nomura@pobox.com>
2014-11-06 03:28:32 +0900

    changed tablacus to use install chocolatey zip package command.

--- automatic/tablacus/tools/chocolateyInstall.ps1
+++ automatic/tablacus/tools/chocolateyInstall.ps1
@@ -1,13 +1,4 @@
 ﻿$cname = "{{PackageName}}"
-#try {
-  $downUrl = "{{DownloadUrl}}"
-  $parentpath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  Get-ChocolateyWebFile $cname "${parentpath}\{{PackageName}}.zip" "$downUrl"
-  $cmd7z = "${Env:ProgramFiles}\7-Zip\7z.exe"
-  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$parentpath" -ArgumentList "x -y ${parentpath}\{{PackageName}}.zip"
-
-  Write-ChocolateySuccess $cname
-#} catch {
-#  Write-ChocolateyFailure $cname "$($_.Exception.Message)"
-#  throw 
-#}
+$downUrl = "{{DownloadUrl}}"
+$parentpath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage $cname $downUrl $parentPath
