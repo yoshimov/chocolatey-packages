de9f4015fce08fd03f6dd52c6f75aac7d0e64876
NOMURA Yoshihide <nomura@pobox.com>
2014-11-29 03:24:05 +0900

    updated freefilesync to use specific mirror url.

--- automatic/freefilesync/tools/chocolateyInstall.ps1
+++ automatic/freefilesync/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿#try {
-#  $downUrl = '{{DownloadUrl}}'
-  $downUrl = 'http://sourceforge.net/projects/freefilesync/files/FreeFileSync/{{PackageVersion}}/FreeFileSync_{{PackageVersion}}_Windows_Setup.exe/download'
+  $downUrl = '{{DownloadUrl}}'
+#  $downUrl = 'http://downloads.sourceforge.net/project/freefilesync/FreeFileSync/{{PackageVersion}}/FreeFileSync_{{PackageVersion}}_Windows_Setup.exe?r=&ts=&use_mirror=auto'
   Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
   
   # the following is all part of error handling
