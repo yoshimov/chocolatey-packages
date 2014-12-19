3cd566d9e331a9280a7caecdc7dbab927978f6c2
NOMURA Yoshihide <nomura@pobox.com>
2014-12-20 02:46:29 +0900

    updated groovy package.

--- automatic/groovy/tools/chocolateyInstall.ps1
+++ automatic/groovy/tools/chocolateyInstall.ps1
@@ -1,21 +1,8 @@
-﻿try {
-  $package = 'groovy-{{PackageVersion}}'
+﻿$package = 'groovy-{{PackageVersion}}'
+$binRoot = "$env:systemdrive\"
+if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+$installDir = Join-Path $binRoot $package
+$zipUrl = '{{DownloadUrl}}'
 
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
-  ### For BinRoot, use the following instead ###
-  $binRoot = "$env:systemdrive\"
-  ### Using an environment variable to to define the bin root until we implement configuration ###
-  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
-  $installDir = Join-Path $binRoot $package
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  $zipUrl = '{{DownloadUrl}}'
-
-  Install-ChocolateyZipPackage 'groovy' "$zipUrl" "$binRoot"
-  
-  Install-ChocolateyPath "$installDir\bin" 'User' # Machine will assert administrative rights
-
-  Write-ChocolateySuccess 'groovy'
-} catch {
-  Write-ChocolateyFailure 'groovy' "$($_.Exception.Message)"
-  throw 
-}
+Install-ChocolateyZipPackage 'groovy' "$zipUrl" "$binRoot"
+Install-ChocolateyPath "$installDir\bin" 'User'
--- automatic/groovy/tools/chocolateyUninstall.ps1
+++ automatic/groovy/tools/chocolateyUninstall.ps1
@@ -1,14 +1,5 @@
-﻿try {
-  $package = 'groovy-{{PackageVersion}}'
-  $binRoot = "$env:systemdrive\"
-  ### Using an environment variable to to define the bin root until we implement configuration ###
-  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
-  $installDir = Join-Path $binRoot $package
-
-  Remove-Item -Path "$installDir" -Force -Recurse
-
-  Write-ChocolateySuccess 'groovy'
-} catch {
-  Write-ChocolateyFailure 'groovy' "$($_.Exception.Message)"
-  throw 
-}
+﻿$package = 'groovy-{{PackageVersion}}'
+$binRoot = "$env:systemdrive\"
+if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+$installDir = Join-Path $binRoot $package
+Remove-Item -Path "$installDir" -Force -Recurse
