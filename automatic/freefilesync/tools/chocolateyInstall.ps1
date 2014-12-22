650e58ce368ce9e2fe183d330c10e6eb7d78d59b
Nomura, Yoshihide <y.nomura@jp.fujitsu.com>
2014-12-22 10:06:47 +0900

    removed comment from freefilesync. removed dependency from tablacus to 7zip

--- automatic/freefilesync/tools/chocolateyInstall.ps1
+++ automatic/freefilesync/tools/chocolateyInstall.ps1
@@ -1,11 +1,2 @@
-﻿#try {
-  $downUrl = '{{DownloadUrl}}'
-#  $downUrl = 'http://downloads.sourceforge.net/project/freefilesync/FreeFileSync/{{PackageVersion}}/FreeFileSync_{{PackageVersion}}_Windows_Setup.exe?r=&ts=&use_mirror=auto'
-  Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
-  
-  # the following is all part of error handling
-#  Write-ChocolateySuccess '{{PackageName}}'
-#} catch {
-#  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
-#  throw 
-#}
+﻿$downUrl = '{{DownloadUrl}}'
+Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
--- automatic/freefilesync/tools/chocolateyUninstall.ps1
+++ automatic/freefilesync/tools/chocolateyUninstall.ps1
@@ -1,10 +1,2 @@
-﻿try {
-  $unpath = "${Env:ProgramFiles}\FreeFileSync\uninstall.exe"
-  Uninstall-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$unpath" -validExitCodes @(0)
-  
-  # the following is all part of error handling
-  Write-ChocolateySuccess '{{PackageName}}'
-} catch {
-  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
-  throw 
-}
+﻿$unpath = "${Env:ProgramFiles}\FreeFileSync\uninstall.exe"
+Uninstall-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$unpath" -validExitCodes @(0)
