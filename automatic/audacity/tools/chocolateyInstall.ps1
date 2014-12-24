a3149203874afbf4c37eacb26e91b52923504974
NOMURA Yoshihide <nomura@pobox.com>
2014-12-25 02:52:58 +0900

    changed install command option of audacity.

--- automatic/audacity/tools/chocolateyInstall.ps1
+++ automatic/audacity/tools/chocolateyInstall.ps1
@@ -1,11 +1,3 @@
-﻿try {
-  $downUrl = '{{DownloadUrl}}'
+﻿  $downUrl = '{{DownloadUrl}}'
   # installer, will assert administrative rights
-  Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/SILENT' "$downUrl" -validExitCodes @(0)
-
-  # the following is all part of error handling
-  Write-ChocolateySuccess '{{PackageName}}'
-} catch {
-  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
-  throw 
-}
+  Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/VERYSILENT /NORESTART' "$downUrl" -validExitCodes @(0)
--- automatic/audacity/tools/chocolateyUninstall.ps1
+++ automatic/audacity/tools/chocolateyUninstall.ps1
@@ -1,5 +1,4 @@
-﻿try {
-  $processor = Get-WmiObject Win32_Processor
+﻿  $processor = Get-WmiObject Win32_Processor
   $is64bit = $processor.AddressWidth -eq 64
   if ($is64bit) {
     $unpath = "${Env:ProgramFiles(x86)}\Audacity\unins000.exe"
@@ -7,10 +6,3 @@
     $unpath = "${Env:ProgramFiles}\Audacity\unins000.exe"
   }
   Uninstall-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$unpath" -validExitCodes @(0)
-  
-  # the following is all part of error handling
-  Write-ChocolateySuccess '{{PackageName}}'
-} catch {
-  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
-  throw 
-}
