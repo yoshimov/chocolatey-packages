5dfcebc7ab7a67757727c0ab4902114acc824476
BrandonG777 <brandongabbard@gmail.com>
2015-05-08 16:01:31 -0500

    Updated for version 1.2.0.5

---
+++ ultravnc/tools/chocolateyUninstall.ps1
@@ -0,0 +1,27 @@
+﻿#try {
+#  $unpath = "${Env:ProgramFiles}\UltraVNC\unins000.exe"
+#  Uninstall-ChocolateyPackage 'ultravnc' 'EXE' '/SILENT' "$unpath" -validExitCodes @(0)
+  
+  
+  #NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+
+$packageName = 'ultravnc'
+$registryUninstallerKeyName = 'Ultravnc2_is1' #ensure this is the value in the registry
+$installerType = 'EXE'
+$silentArgs = '/SILENT'
+$validExitCodes = @(0)
+
+$osBitness = Get-ProcessorBits
+ 
+#if ($osBitness -eq 64) {
+#  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+#} else {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+#} 
+	
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
---
+++ ultravnc/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'ultravnc' # arbitrary name for the package, used in messages
+$registryUninstallerKeyName = 'ultravnc' #ensure this is the value in the registry
+$installerType = 'EXE' #only one of these: exe, msi, msu
+$url = 'http://www.uvnc.eu/download/1205/UltraVNC_1_2_05_X86_Setup.exe' # download url
+$url64 = 'http://www.uvnc.eu/download/1205/UltraVNC_1_2_05_X64_Setup.exe' # 64bit URL here or remove - if installer decides, then use $url
+$silentArgs = '/SILENT' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
