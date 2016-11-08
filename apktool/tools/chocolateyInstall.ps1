efbf4505f281f96f460f27296d827b27920bc33e
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-11-08 10:13:51 +0300

    Update chocolateyInstall.ps1

--- apktool/tools/chocolateyInstall.ps1
+++ apktool/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿try {
-  $downUrl = 'http://android-apktool.googlecode.com/files/apktool1.5.2.tar.bz2'
-  $downHelperUrl = 'http://android-apktool.googlecode.com/files/apktool-install-windows-r05-ibot.tar.bz2'
+  $downUrl = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/android-apktool/apktool1.5.2.tar.bz2'
+  $downHelperUrl = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/android-apktool/apktool-install-windows-r05-ibot.tar.bz2'
   $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   
   Get-ChocolateyWebFile 'apktool' "${toolsDir}\apktool.tbz" "$downUrl"
