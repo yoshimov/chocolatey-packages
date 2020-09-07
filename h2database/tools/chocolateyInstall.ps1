660cd1e1bb63cf47976043f1c5368cc00df338be
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-09-07 11:45:36 +0200

    h2database now maintained by tunisiano

--- h2database/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿#try {
-  $downUrl = 'http://www.h2database.com/h2-setup-2015-03-02.exe'
-  Install-ChocolateyPackage 'h2database' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
-
-  #Write-ChocolateySuccess 'h2database'
-#} catch {
-  #Write-ChocolateyFailure 'h2database' "$($_.Exception.Message)"
-  #throw 
-#}
